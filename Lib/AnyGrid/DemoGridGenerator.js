52ad9fce6a5944b0fed87a3f17595feb142af176
artyv <outring@gmail.com>
2012-07-08 01:51:17 +0600

    Demo grid generator finalization

--- Lib/AnyGrid/DemoGridGenerator.js
+++ Lib/AnyGrid/DemoGridGenerator.js
@@ -1,21 +1,36 @@
 define(["./AnyGridDescriptor"], function (AnyGridDescriptor) {
 
     function DemoGridGenerator(options) {
-        this.__options = options;
-        this.__init();
+        this.__init(options);
     }
 
     DemoGridGenerator.prototype = {
-        __init: function () {
+        __init: function (options) {
+            this.__options = options;
             this.__columns = [];
             this.__descriptor = new AnyGridDescriptor(this.__options.containerColumnsCount);
             this.__gridColumnsCount = this.__options.gridColumnsCount;
             this.__gutterWidth = this.__options.gutterWidth;
-            this.__updateGrid();
-            this.__container = document.createElement("div");
+            this.__createContainer();
             this.__createColumnTemplate();
+            this.__updateGrid();
         },
-        get: function () {
+        __createContainer: function () {
+            this.__container = document.createElement("div");
+            this.__container.className = this.__options.containerClass;
+            this.__containerPlaceholder = document.createElement("div");
+        },
+        __createColumnTemplate: function () {
+            this.__columnTemplate = document.createElement("div");
+            this.__columnTemplate.className = this.__options.columnClass;
+        },
+        __createColumn: function () {
+            return this.__columnTemplate.cloneNode(false);
+        },
+        getGrid: function () {
+            return this.__grid;
+        },
+        getContainer: function () {
             return this.__container;
         },
         setColumnsCount: function (count) {
@@ -28,10 +43,15 @@ define(["./AnyGridDescriptor"], function (AnyGridDescriptor) {
             this.__updateGrid();
         },
         __updateGrid: function () {
-            this.__grid = this.__descriptor.getGrid(this.__gridColumnsCount, this.__gutterWidth);
-            this.__updateGridView();
+            try {
+                this.__grid = this.__descriptor.getGrid(this.__gridColumnsCount, this.__gutterWidth);
+                this.__updateGridView();
+            }
+            catch (e) {}
         },
         __updateGridView: function () {
+            this.__detachContainer();
+            this.__container.style.marginRight = 100 - this.__grid.getContainerWidth() + "%";
             var generatedCount = this.__columns.length;
             for (var i = 0, l = Math.max(this.__gridColumnsCount, generatedCount); i < l; i++) {
                 if (i >= generatedCount) {
@@ -40,24 +60,40 @@ define(["./AnyGridDescriptor"], function (AnyGridDescriptor) {
                 }
                 if (i < this.__gridColumnsCount) {
                     this.__columns[i].style.display = "";
-                    this.__setColumnStyle(this.__columns[i], i);
+                    this.__setColumnStyle(this.__columns[i], i + 1);
                 }
                 else
                     this.__columns[i].style.display = "none";
             }
+            this.__attachContainer();
         },
-        __setColumnStyle: function (column, index) {
+        __detachContainer: function () {
+            this.__switchNodes(this.__container, this.__containerPlaceholder);
+        },
+        __attachContainer: function () {
+            this.__switchNodes(this.__containerPlaceholder, this.__container);
+        },
+        __switchNodes: function (removingNode, addingNode) {
+            if (!removingNode.parentNode)
+                return;
+            removingNode.parentNode.insertBefore(addingNode, removingNode);
+            removingNode.parentNode.removeChild(removingNode);
+        },
+        __setColumnStyle: function (column, offset) {
             var blockWidth = this.__grid.getBlockWidth(1);
-            column.style.left = this.__grid.getBlockOffset(index) + "%";
-            column.style.marginRight = blockWidth * 1 + "%";
+            column.style.left = this.__grid.getBlockOffset(offset) + "%";
+            column.style.marginRight = blockWidth * -1 + "%";
             column.style.width = blockWidth + "%";
+            this.__toggleColumnAlternativeClass(column, offset);
         },
-        __createColumnTemplate: function () {
-            this.__columnTemplate = document.createElement("div");
-            this.__columnTemplate.className = this.__options.columnClass;
-        },
-        __createColumn: function () {
-            return this.__columnTemplate.cloneNode(false);
+        __toggleColumnAlternativeClass: function (column, offset) {
+            var alternativeClass = this.__options.alternativeColumnClass;
+            if (this.__gutterWidth < 2 && offset % 2 === 0) {
+                if (column.className.indexOf(alternativeClass) === -1)
+                    column.className += " " + alternativeClass;
+            }
+            else
+                column.className = column.className.replace(alternativeClass, "");
         }
     };
 
--- MainPage.js
+++ MainPage.js
@@ -1,87 +1,45 @@
 define(
     [
         'js!' + 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js',
-        './Lib/AnyGrid/AnyGridDescriptor',
+        './Lib/AnyGrid/DemoGridGenerator',
         './Lib/AnyGrid/CssGenerator'
     ],
-    function (jQueryLoaded, AnyGridDescriptor, CssGenerator) {
+    function (jQueryLoaded, DemoGridGenerator, CssGenerator) {
 
         function MainPage() {
-            var defaultContainerCols = 5;
-            var defaultGridCols = 12;
-            var defaultGutterWidth = 5;
-
-            var descriptor = new AnyGridDescriptor(defaultContainerCols);
-            var grid = descriptor.getGrid(defaultGridCols, defaultGutterWidth);
-
-            var $preview = $('#Preview');
-            var cols = $preview.children();
-            var $columnsNumberInput = $('#ColumnsNumberInput');
-            var $gutterWidthInput = $('#GutterWidthInput');
-            var $colTemplate = $('<div />', { class: "preview-col" });
-
-            function generateCols(colsCount) {
-                for (var i = 1; i <= colsCount; i++) {
-                    var $col = $colTemplate.clone().appendTo($preview);
-                    cols.push($col);
-                }
-            }
-
-            function fixView(colsCount, gutterWidth) {
-                for (var i = 1, l = cols.length; i <= l; i++) {
-                    var $col = cols[i - 1];
-                    if (i <= colsCount)
-                        $col
-                            .css({
-                                left: grid.getBlockOffset(i) + "%",
-                                marginRight: -grid.getBlockWidth(1) + "%",
-                                width: grid.getBlockWidth(1) + "%"
-                            })
-                            .toggleClass("preview-col_alternative", gutterWidth === 0 && i % 2 === 0)
-                            .show();
-                    else
-                        $col.hide()
-                }
-            }
-
-            function regenerate() {
-                var newColsCount = $columnsNumberInput.val();
-                var newGutterWidth = $gutterWidthInput.val();
-                if (newColsCount === "" || newGutterWidth === "")
-                    return;
-                newColsCount = parseInt(newColsCount);
-                newGutterWidth = parseInt(newGutterWidth);
-                if (isNaN(newColsCount) || isNaN(newGutterWidth))
-                    return;
-                if (newGutterWidth < 0)
-                    return;
-                try {
-                    grid = descriptor.getGrid(newColsCount, newGutterWidth);
-                    $preview.css({ marginRight: 100 - grid.getContainerWidth() + "%" });
-                    var $previewPlaceholder = $preview.after('<div />').next();
-                    $preview.detach();
-                    if (newColsCount > cols.length)
-                        generateCols(newColsCount - cols.length);
-                    fixView(newColsCount, newGutterWidth);
-                    $previewPlaceholder.after($preview);
-                    $previewPlaceholder.remove();
-                }
-                catch (e) {
-                }
-            }
-
-            $columnsNumberInput.add($gutterWidthInput).bind('keyup change', regenerate);
-
-            $columnsNumberInput.val(defaultGridCols);
-            $gutterWidthInput.val(defaultGutterWidth);
-
-            regenerate();
-
-            $('#GetCodeButton').click(function () {
-                var generator = new CssGenerator(grid, { prefix: "g-" });
-                var win = window.open('data:text/css,' + encodeURIComponent(generator.getCode()));
-            });
+            this.__init();
         }
+
+        MainPage.prototype = {
+            __init: function() {
+                this.__$columnsCountInput = $('#ColumnsNumberInput');
+                this.__$gutterWidthInput = $('#GutterWidthInput');
+                this.__$getCodeButton = $('#GetCodeButton');
+                this.__generator = new DemoGridGenerator({
+                    containerColumnsCount: 5,
+                    gridColumnsCount: this.__$columnsCountInput.val(),
+                    gutterWidth: this.__$gutterWidthInput.val(),
+                    containerClass: "preview-content",
+                    columnClass: "preview-col",
+                    alternativeColumnClass: "preview-col_alternative"
+                });
+                $("#Preview").append(this.__generator.getContainer());
+                this.__attachEventHandlers();
+            },
+            __attachEventHandlers: function() {
+                var self = this;
+                this.__$columnsCountInput.bind('keyup change', function() {
+                    self.__generator.setColumnsCount(this.value);
+                });
+                this.__$gutterWidthInput.bind('keyup change', function() {
+                    self.__generator.setGutterWidth(this.value);
+                });
+                this.__$getCodeButton.click(function () {
+                    var generator = new CssGenerator(this.__generator.getGrid(), { prefix: "g-" });
+                    window.open('data:text/css,' + encodeURIComponent(generator.getCode()));
+                });
+            }
+        };
 
         return MainPage;
     });
